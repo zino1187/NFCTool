53559b97321d58a695d47acf9df9c85470a9283e
zino1187 <zino1187@gmail.com>
2016-06-29 18:32:59 +0900

    hh

--- app/src/main/java/com/example/zino/nfctool/MainActivity.java
+++ app/src/main/java/com/example/zino/nfctool/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.zino.nfctool;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,9 +14,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        viewPager =(ViewPager) findViewById(R.id.viewPager);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
         myPagerAdapter = new MyPagerAdapter(getSupportFragmentManager());
 
         viewPager.setAdapter(myPagerAdapter);
     }
+
+    public void btnClick(View view) {
+        if (view.getId() == R.id.bt_read) {
+            viewPager.setCurrentItem(0); //읽기용 프레그먼트로 이동
+        } else if (view.getId() == R.id.bt_write) {
+            viewPager.setCurrentItem(1); //쓰기용 프레그먼트로 이동
+        }
+    }
 }
+
+
+
+
+
+
--- app/src/main/java/com/example/zino/nfctool/ReadFragment.java
+++ app/src/main/java/com/example/zino/nfctool/ReadFragment.java
@@ -6,17 +6,37 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 /*
  * Created by zino on 2016-06-28.
  */
-public class ReadFragment extends Fragment {
-    @Nullable
-    @Override
+public class ReadFragment extends Fragment implements View.OnClickListener{
+    Button read_start;
+    TextView txt_data;
+    ProgressBar bar;
+
+
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = null;
         view = inflater.inflate(R.layout.read_layout, container, false);
+
+        read_start = (Button)view.findViewById(R.id.read_start);
+        txt_data=(TextView)view.findViewById(R.id.txt_data);
+        bar=(ProgressBar)view.findViewById(R.id.bar);
+
+        //버튼과 리스너와의 연결
+        read_start.setOnClickListener(this);
+
         return view;
+    }
+
+    public void onClick(View view) {
+        //프로그래스바 보이기!!
+        bar.setVisibility(View.VISIBLE);
+
     }
 }
 
